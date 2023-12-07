8cb9a862dba6b66795836230428390c16b1424d8
mani-prakash-reddy <maniprakashreddy223230@gmail.com>
2023-12-07 22:53:13 +0530

    updated setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,13 @@
 sudo apt-get update
 
 # Install PostgreSQL and pgAdmin
-sudo apt-get install -y postgresql postgresql-contrib pgadmin4
+sudo apt-get install postgresql postgresql-contrib pgadmin4
 
 # Create a new PostgreSQL database named "predico"
 sudo -u postgres createdb predico
+
+# Install Python and pip
+sudo apt-get install python3.10 python3-pip
 
 # Install the project requirements
 pip install -r requirements.txt
