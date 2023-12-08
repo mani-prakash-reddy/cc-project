8a4900b91af957edd0f176629507f4001de6128d
mani-prakash-reddy <maniprakashreddy223230@gmail.com>
2023-12-08 09:35:11 +0530

    Update database settings to use PostgreSQL

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -83,8 +83,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'predico',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
